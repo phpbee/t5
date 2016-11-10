66b6ad51bf95778f5119af932004c0d84cd577a3
alexk <alex@kochetov.com>
2016-11-10 18:32:53 +0300

    WiP

--- src/main/java/org/phpbee/t5/Transaction.java
+++ src/main/java/org/phpbee/t5/Transaction.java
@@ -1,8 +1,20 @@
 package org.phpbee.t5;
 
+import javax.persistence.*;
+
+@Entity
+@Table( name = "transactions" )
+@NamedQueries({
+        @NamedQuery(name = "Transaction.findById", query = "select id from Transaction where id = 5")
+})
 public class Transaction {
 
-    private final long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    protected Transaction() {
+    }
 
     public Transaction(long id) {
         this.id = id;
--- src/main/java/org/phpbee/t5/TransactionController.java
+++ src/main/java/org/phpbee/t5/TransactionController.java
@@ -1,5 +1,7 @@
 package org.phpbee.t5;
 
+import org.phpbee.t5.dao.TransactionDao;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,4 +18,10 @@ public class TransactionController {
     public Transaction transaction() {
         return new Transaction(counter.incrementAndGet());
     }
+
+    @RequestMapping(value="/transaction/{id}", method=RequestMethod.GET)
+    public Transaction transaction(TransactionDao transactionDao, @PathVariable("id") Long id) {
+        return transactionDao.findById(id);
+    }
+
 }
