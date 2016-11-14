e84e9bd8e618b3c11fac65cef7f106098c3091d7
alexk <alex@kochetov.com>
2016-11-14 13:03:30 +0300

    POST /transaction

--- src/main/java/org/phpbee/t5/Transaction.java
+++ src/main/java/org/phpbee/t5/Transaction.java
@@ -1,8 +1,22 @@
 package org.phpbee.t5;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "transaction")
 public class Transaction {
 
-    private final long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "created", updatable = false, insertable = false, nullable = false, columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created;
+
+
+    public Transaction () { }
 
     public Transaction(long id) {
         this.id = id;
